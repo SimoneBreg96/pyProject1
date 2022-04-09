002c8c7336a9232d6fd69668ca85f56117efc1ac
SimoneBreg96 <simonebreg@gmail.com>
2022-04-09 21:15:21 +0200

    show colored bottons

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # I have disabled Pylance VS extension..
 import kivy
+kivy.require('2.1.0') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-kivy.require('2.1.0') # replace with your current kivy version !
+import random
+from widgets import BoxLayoutApp
 
-class MainWindow(BoxLayout):
-    pass
 
 class MyApp(App):
     def build(self):
         return MainWindow()
 
 
-MyApp().run()
+BoxLayoutApp().run()
