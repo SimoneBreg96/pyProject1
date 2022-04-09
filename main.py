d2364de35f82ffe980acec9be6a88e4efe26c2f0
SimoneBreg96 <simonebreg@gmail.com>
2022-04-09 21:50:07 +0200

    working base style layout

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ kivy.require('2.1.0') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 import random
-from widgets import BoxLayoutApp
 
+class MainWindow(BoxLayout):
+    pass
 
-class MyApp(App):
+class MainPage(App):
     def build(self):
         return MainWindow()
 
-
-BoxLayoutApp().run()
+MainPage().run()
+# BoxLayoutApp().run()
